ab6b668e0e2cf5f6b74f79c02f6cf38d359de3f2
Andrew1996-la <asoborev@mail.ru>
2022-09-07 01:01:13 +0300

    Добавил компонет Dialogs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Header from './components/Header'
 import Navigation from './components/Navigation'
 import Profile from './components/profile/Profile'
 import React from 'react'
+import Dialogs from './components/dialogs/Dialogs'
 
 function App() {
     return (
         <div className="grid grid-cols-[2fr,_10fr] grid-rows-[120px,_100%]">
             <Header />
             <Navigation />
-            <Profile />
+            {/*<Profile />*/}
+            <Dialogs />
         </div>
     )
 }
---
+++ src/components/dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Dialogs extends Component {
+    render() {
+        return (
+            <div className="bg-mainGreen grid grid-rows-rowsProfileSection grid-cols-[3.5fr,_8fr]">
+                <h2>it is dialogs</h2>
+            </div>
+        )
+    }
+}
+
+export default Dialogs
